402b0362cf4850c6b16a3cffe276337826796f6a
Javierif <javier.ifer@gmail.com>
2016-07-01 12:25:03 +0200

    Add files via upload
    Updated simple VB with google maps

---
+++ VisualBasic_SimpleGoogleMaps/Form1.Designer.vb
@@ -0,0 +1,108 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Lat = New System.Windows.Forms.TextBox
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Lng = New System.Windows.Forms.TextBox
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.SuspendLayout()
+        '
+        'Lat
+        '
+        Me.Lat.Location = New System.Drawing.Point(25, 39)
+        Me.Lat.Name = "Lat"
+        Me.Lat.Size = New System.Drawing.Size(294, 20)
+        Me.Lat.TabIndex = 0
+        Me.Lat.Text = "37.9863068"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(25, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Latitud"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(392, 20)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(48, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Longitud"
+        '
+        'Lng
+        '
+        Me.Lng.Location = New System.Drawing.Point(392, 39)
+        Me.Lng.Name = "Lng"
+        Me.Lng.Size = New System.Drawing.Size(294, 20)
+        Me.Lng.TabIndex = 2
+        Me.Lng.Text = "-1.1281766"
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Location = New System.Drawing.Point(25, 85)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(661, 322)
+        Me.WebBrowser1.TabIndex = 4
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(244, 432)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(243, 23)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Generar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(712, 477)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Lng)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Lat)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Lat As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Lng As System.Windows.Forms.TextBox
+    Friend WithEvents WebBrowser1 As System.Windows.Forms.WebBrowser
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
---
+++ VisualBasic_SimpleGoogleMaps/Form1.vb
@@ -0,0 +1,61 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        Dim webHTML As String
+        webHTML = genHead() & genScript(Lat.Text, Lng.Text) & genBody()
+        WebBrowser1.Navigate("about:blank")
+        WebBrowser1.Document.OpenNew(False)
+        WebBrowser1.Document.Write(webHTML)
+        WebBrowser1.Refresh()
+    End Sub
+
+    Private Function genHead()
+        Dim head As String = <![CDATA[
+            <!DOCTYPE html>
+            <html>
+                <head>
+                    <title>Simple Map</title>
+                    <meta name="viewport">
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                    <style>
+                        html, body, #map-canvas {
+                            margin:0;
+                            padding:0;
+                            padding:0;
+                            height:100%;
+                        }
+                    </style>
+                    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+         ]]>.Value()
+        Return head
+    End Function
+
+    Private Function genBody()
+        Dim body As String = <![CDATA[
+            <body>
+                <div id="map-canvas"></div>
+            </body>
+        </html>
+        ]]>.Value()
+        Return body
+    End Function
+
+    Private Function genScript(ByVal lat, ByVal lng)
+        Dim scMap As String = <![CDATA[
+            <script>
+                var map;
+                function initialize() {
+        ]]>.Value()
+
+        Dim scMapv2 As String = "var mapOptions= {zoom: 13,mapTypeId: google.maps.MapTypeId.ROADMAP,center: new google.maps.LatLng(" + Trim(lat) + "," + Trim(lng) + ")}"
+
+        Dim scMapv3 As String = <![CDATA[
+                    map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+                }
+                google.maps.event.addDomListener(window, 'load', initialize);
+            </script>
+        ]]>.Value()
+        Return scMap & scMapv2 & scMapv3
+    End Function
+End Class
